d80c692be6afc07c2fa4940bfb28cb35b82d8441
yejie.wen <yejie.wen@hoo.com>
2021-01-05 15:15:33 +0800

    测试

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,6 @@
 declare module '*.vue' {
+  // eslint-disable-next-line prettier/prettier
   import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
+  const component: DefineComponent<{}, {}, never>
   export default component
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,10 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <button @click="open = !open">Button</button>
+    <teleport to="body">
+      <div class="dialog"></div>
+    </teleport>
   </div>
 </template>
 
@@ -14,5 +18,20 @@ import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
     HelloWorld
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  open!: false;
+}
 </script>
+
+<style scoped type="scss">
+.dialog {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  border: 1px solid #dcdcdc;
+  width: 360px;
+  height: 200px;
+}
+</style>
