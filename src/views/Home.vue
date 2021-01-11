850924fe1b8a6d14928db23a0905e2bbeea0ed2e
yejie.wen <yejie.wen@hoo.com>
2021-01-11 14:15:44 +0800

    相关测试

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,6 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-    <button @click="open = !open">Button</button>
-    <teleport to="body">
-      <div class="dialog"></div>
-    </teleport>
   </div>
 </template>
 
@@ -18,20 +14,5 @@ import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
     HelloWorld
   }
 })
-export default class Home extends Vue {
-  open!: false;
-}
+export default class Home extends Vue {}
 </script>
-
-<style scoped type="scss">
-.dialog {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: #fff;
-  border: 1px solid #dcdcdc;
-  width: 360px;
-  height: 200px;
-}
-</style>
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pluginOptions: {
+    "vue-loader": {
+      compilerOptions: {
+        preserveWhitespace: false
+      }
+    }
+  }
+};
