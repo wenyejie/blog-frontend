42e4bf4e1e586f53c02836813e13611ab636cd06
yejie.wen <yejie.wen@hoo.com>
2021-02-02 14:54:30 +0800

    新增icon组件

---
+++ src/components/icon/icon.vue
@@ -0,0 +1,43 @@
+<template>
+  <i class="s-icon" :class="classes" :style="styles"></i>
+</template>
+
+<script>
+import { computed, defineComponent } from 'vue'
+import { toCSSUnit } from '../utils'
+export default defineComponent({
+  name: 'SIcon',
+  props: {
+    type: String,
+    nativeClass: String,
+    size: [Number, String],
+    color: String
+  },
+  setup(props) {
+    const classes = computed(() => [
+      props.nativeClass,
+      {
+        [`s-icon-${props.type}`]: !!props.type
+      }
+    ])
+
+    const styles = computed(() => {
+      const result = {}
+      if (props.color) {
+        result.color = props.color
+      }
+      if (props.size) {
+        result.fontSize = toCSSUnit(props.size)
+      }
+      return result
+    })
+
+    return {
+      classes,
+      styles
+    }
+  }
+})
+</script>
+
+<style lang="scss" src="./icon.scss"></style>
---
+++ src/components/icon/index.ts
@@ -0,0 +1,1 @@
+export default () => import(/* webpackChunkName: 's-icon' */ './icon.vue')
