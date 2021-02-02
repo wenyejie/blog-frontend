f676c8b986a516def881620ea33f07b00981873b
yejie.wen <yejie.wen@hoo.com>
2021-02-02 18:27:57 +0800

    优化分类操作

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -3,7 +3,11 @@
     <h1 class="page-title">分类管理</h1>
     <form class="manage-category--add" @submit.prevent>
       <s-input class="manage-category--add--input" v-model.trim="newCategory" />
-      <s-button native-type="submit" class="category--add--btn" @click="handleAddCategory"
+      <s-button
+        native-type="submit"
+        class="category--add--btn"
+        :disabled="adding"
+        @click="handleAddCategory"
         >添加</s-button
       >
     </form>
@@ -11,8 +15,10 @@
       <ul class="manage-category--list">
         <li class="manage-category--item" v-for="(item, index) in categoryList" :key="item._id">
           <s-input v-model.trim="item.label" />
-          <s-button @click="handleDeleteCategory(item, index)">删除</s-button>
-          <s-button @click="handleUpdateCategory(item)">更新</s-button>
+          <s-button :disabled="item.disabled" @click="handleDeleteCategory(item, index)"
+            >删除</s-button
+          >
+          <s-button :disabled="item.disabled" @click="handleUpdateCategory(item)">更新</s-button>
         </li>
       </ul>
     </form>
@@ -32,42 +38,59 @@ export default defineComponent({
     const categoryList = fetchCategoryList()
 
     const newCategory = ref('')
+    const adding = ref(false)
 
     // 添加分类
     const handleAddCategory = () => {
-      addCategory({ label: newCategory.value }).then((category: any) => {
-        $success('添加成功')
-        categoryList.push(category)
-        newCategory.value = ''
-      })
+      adding.value = true
+      addCategory({ label: newCategory.value })
+        .then((category: any) => {
+          $success('添加成功')
+          categoryList.push(category)
+          newCategory.value = ''
+        })
+        .finally(() => {
+          adding.value = false
+        })
     }
 
     // 更新分类
     const handleUpdateCategory = (item: Category) => {
-      updateCategory(item).then(() => {
-        $success('更新成功')
-      })
+      item.updating = true
+      updateCategory(item)
+        .then(() => {
+          $success('更新成功')
+        })
+        .finally(() => {
+          item.updating = false
+        })
     }
 
     // 删除分类
     const handleDeleteCategory = (item: Category, index: number, reconfirm = false) => {
-      deleteCategory({ _id: item._id, reconfirm }).then(
-        () => {
-          categoryList.splice(index, 1)
-          $success('删除成功')
-        },
-        response => {
-          // 如果分类下有文章并且确认删除
-          if (response?.code === '005001') {
-            if (window.confirm(response?.message)) {
-              handleDeleteCategory(item, index, true)
+      item.deleting = true
+      deleteCategory({ _id: item._id, reconfirm }, { disabledTip: '005001' })
+        .then(
+          () => {
+            categoryList.splice(index, 1)
+            $success(`标签"${item.label}"删除成功`)
+          },
+          response => {
+            // 如果分类下有文章并且确认删除
+            if (response?.code === '005001') {
+              if (window.confirm(response?.message)) {
+                handleDeleteCategory(item, index, true)
+              }
             }
           }
-        }
-      )
+        )
+        .finally(() => {
+          item.deleting = false
+        })
     }
 
     return {
+      adding,
       newCategory,
       categoryList,
       handleAddCategory,
