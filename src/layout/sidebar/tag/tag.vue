fce8b7efa17d46176e3c3b9c9d2bc83a03cf26e7
yejie.wen <yejie.wen@hoo.com>
2021-02-03 11:29:02 +0800

    调整标签统计样式

--- src/layout/sidebar/tag/tag.vue
+++ src/layout/sidebar/tag/tag.vue
@@ -1,23 +1,34 @@
 <template>
   <s-sidebar-item title="标签统计" class="s-tag">
     <ol class="s-tag--list">
-      <li class="s-tag--item">
-        <router-link to="/tag/javascript">javascript</router-link>
-      </li>
-      <li class="s-tag--item">
-        <router-link to="/tag/javascript">javascript</router-link>
+      <li class="s-tag--item" v-for="item in tagList" :key="item._id">
+        <router-link :to="`/tag/${item.label}`"
+          >{{ item.label }}<span class="s-tag--count">{{ item.count }}</span></router-link
+        >
       </li>
     </ol>
   </s-sidebar-item>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
+import { useStore } from 'vuex'
 import SSidebarItem from '../sidebarItem'
 
 export default defineComponent({
   name: 'STag',
-  components: { SSidebarItem }
+  components: { SSidebarItem },
+  setup() {
+    const store = useStore()
+    const tagList = computed(() => {
+      return store.state.tag.countList
+    })
+    store.dispatch('getTagCount')
+
+    return {
+      tagList
+    }
+  }
 })
 </script>
 
