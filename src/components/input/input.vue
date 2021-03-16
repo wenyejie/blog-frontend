5261ccccd0a2ea31a2df07cb4e0f2787848953f0
yejie.wen <yejie.wen@hoo.com>
2021-03-16 10:47:31 +0800

    优化input组件, 新增suffix和enter事件

--- src/components/input/input.vue
+++ src/components/input/input.vue
@@ -5,11 +5,14 @@
  -->
 <template>
   <div :class="classes">
+    <slot name="prefix"></slot>
     <input
       :name="name"
+      :title="title"
       v-model="innerValue"
       @input="handleInput"
       @change="handleChange"
+      @keydown.enter="handleEnter"
       :class="`${CLS_NAME}--inner`"
       :placeholder="placeholder"
       :disabled="disabled"
@@ -19,17 +22,24 @@
       :autofocus="autofocus"
       :type="type"
     />
+    <span @click="handleSuffixClick" class="s-input--suffix" v-if="$slots.suffix || suffixIcon">
+      <slot name="suffix">
+        <s-icon v-if="suffixIcon" :type="suffixIcon" class="s-input--icon s-input--suffix-icon" />
+      </slot>
+    </span>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, computed, ref, watch } from 'vue'
 import { propSizeOpts, formEleDefProps } from '@/composition/formElement'
+import SIcon from '../icon'
 
 const CLS_NAME = 's-input'
 
 export default defineComponent({
   name: 'SInput',
+  components: { SIcon },
   props: {
     modelValue: {
       type: [String, Number]
@@ -41,15 +51,20 @@ export default defineComponent({
         return ['text', 'password', 'number', 'tel', 'email'].includes(val)
       }
     },
+    // 后缀icon
+    suffixIcon: String,
+    // 前缀icon
+    prefixIcon: String,
     size: propSizeOpts,
     ...formEleDefProps
   },
-  emits: ['update:modelValue', 'input', 'change'],
-  setup(props, { emit }) {
+  emits: ['update:modelValue', 'input', 'change', 'suffixClick', 'enter'],
+  setup(props, { emit, slots }) {
     const classes = computed(() => [
       CLS_NAME,
       {
         'is-disabled': props.disabled,
+        'has-suffix': slots.suffix || props.suffixIcon,
         [`${CLS_NAME}--${props.size}`]: !!props.size
       }
     ])
@@ -72,10 +87,20 @@ export default defineComponent({
       emit('change', event)
     }
 
+    const handleSuffixClick = (event: Event) => {
+      emit('suffixClick', event)
+    }
+
+    const handleEnter = (event: InputEvent) => {
+      emit('enter', event)
+    }
+
     return {
       innerValue,
       handleInput,
       handleChange,
+      handleSuffixClick,
+      handleEnter,
       classes,
       CLS_NAME
     }
