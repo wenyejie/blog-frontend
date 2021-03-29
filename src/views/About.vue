3ed4cf29a479e41329f598b250493eb5466d6a5c
yejie.wen <yejie.wen@hoo.com>
2021-03-29 10:32:12 +0800

    新增关于我

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
-  <h1 class="about--title page-title">关于</h1>
+  <article class="about">
+    <h1 class="about--title page-title">ABOUT ME</h1>
 
-  <p>一个前端开发工程师</p>
+    <p>一个前端开发工程师</p>
+
+    <p>
+      会一点点 <code>HTML</code>, <code>CSS</code>, <code>JavaScript</code>, <code>NodeJS</code>,
+      <code>VUE</code>
+    </p>
+
+    <p>
+      <code>// TODO</code>
+    </p>
+  </article>
 </template>
+
+<style lang="scss" scoped>
+.about {
+  font-size: 18px;
+  p {
+    & + p {
+      margin-top: 1em;
+    }
+  }
+}
+</style>
