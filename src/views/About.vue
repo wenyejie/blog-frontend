81715a464611618f8f86e9f3a0e4236c0177092e
yejie.wen <yejie.wen@hoo.com>
2021-04-02 11:11:48 +0800

    调整关于我

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,12 @@
 <template>
   <article class="about">
-    <h1 class="about--title page-title">ABOUT ME</h1>
+    <h1 class="page-title">ABOUT ME</h1>
 
     <p>一个前端开发工程师</p>
 
     <p>
       会一点点 <code>HTML</code>, <code>CSS</code>, <code>JavaScript</code>, <code>VUE</code>,
-      <code>NodeJS</code>, <code>And more......</code>
+      <code>NodeJS</code>, <code>MongoDB</code>, <code>And more......</code>
     </p>
 
     <p>Email: <a href="mailto:wenyejie@foxmail.com">wenyejie@foxmail.com</a></p>
