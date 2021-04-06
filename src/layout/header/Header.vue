91d9d044de602bef8d15930bd6f9361173a871ae
yejie.wen <yejie.wen@hoo.com>
2021-04-06 15:05:03 +0800

    修复搜索时跳转错误的bug

--- src/layout/header/Header.vue
+++ src/layout/header/Header.vue
@@ -45,7 +45,7 @@ export default defineComponent({
         return
       }
       router.push({
-        path: 'search',
+        path: '/search',
         query: {
           keyword: keyword.value
         }
