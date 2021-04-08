88ce67ee40adbc6a628db0a5a1bb47522a230a58
yejie.wen <yejie.wen@hoo.com>
2021-04-08 16:14:01 +0800

    调整布局使用grid布局

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <s-header />
-  <s-body><router-view /></s-body>
+  <main class="s-main"><router-view /></main>
+  <s-sidebar />
   <s-footer />
 </template>
 
@@ -8,12 +9,14 @@
 import { defineComponent } from 'vue'
 import SHeader from '@/layout/header'
 import SFooter from '@/layout/footer'
-import SBody from '@/layout/body'
+import SSidebar from '@/layout/sidebar'
+// import SBody from '@/layout/body'
 
 export default defineComponent({
   name: 'App',
   components: {
-    SBody,
+    // SBody,
+    SSidebar,
     SHeader,
     SFooter
   }
@@ -28,5 +31,22 @@ export default defineComponent({
   box-shadow: 0 2px 6px rgba(100, 100, 100, 0.3);
   border-radius: var(--border-radius);
   min-width: 1200px;
+  display: grid;
+  grid-template-columns: repeat(24, 4.1666666666666664%);
+  grid-column-gap: 0;
+}
+
+.s-header {
+  grid-column: 1 / 25;
+}
+.s-main {
+  grid-column: 1 / 19;
+}
+.s-sidebar {
+  grid-column: 19 / 25;
+  margin-left: 30px;
+}
+.s-footer {
+  grid-column: 1 / 25;
 }
 </style>
