2165b118d05d48fada1118f104b2cea06965a05a
yejie.wen <yejie.wen@hoo.com>
2021-04-12 17:13:14 +0800

    新增mta统计

---
+++ src/core/statistic.js
@@ -0,0 +1,9 @@
+window._mtac = { performanceMonitor: 1, senseQuery: 1 }
+const mta = document.createElement('script')
+mta.src = '//pingjs.qq.com/h5/stats.js?v2.0.4'
+mta.setAttribute('name', 'MTAH5')
+mta.setAttribute('sid', '500575757')
+mta.setAttribute('cid', '500575758')
+mta.setAttribute('async', 'async')
+const s = document.getElementsByTagName('script')[0]
+s.parentNode.insertBefore(mta, s)
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles'
+import './core/statistic.js'
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .use(components)
-  .mount('#app')
+createApp(App).use(store).use(router).use(components).mount('#app')
